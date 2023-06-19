39f8cf39d8cdeeed6ce107e7f3e7869cd43f08a2
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-19 08:59:12 +0600

    Student add method

--- add.php
+++ add.php
@@ -1,4 +1,21 @@
 <?php include 'inc/header.php'; ?>
+<?php include 'lib/Student.php'; ?>
+
+<?php
+$stu = new Student();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $roll = $_POST['roll'];
+    $insertdata = $stu->insertStudent($name, $roll);
+}
+?>
+
+<?php 
+if(isset($insertdata)){
+    echo $insertdata; 
+}
+?>
+
 
 <div class="panel panel-default">
     <div class="panel-heading">
@@ -9,25 +26,28 @@
     </div>
 
     <div class="panel-body">
-        
+
         <form action="" method="post">
             <br>
-            <div class="form-group"> 
+            <div class="form-group">
                 <h6><label for="name">Student Name</label></h6>
-                <input class="form-control" type="text" name="name" placeholder="Enter Student Name" id="name" required="">
-            </div> 
-            <br>
-            <div class="form-group"> 
-                <h6><label for="roll">Student Roll</label></h6>
-                <input class="form-control" type="text" name="roll"  placeholder="Enter Student Roll" id="roll" required="">
+                <input class="form-control" type="text" name="name" placeholder="Enter Student Name" id="name">
             </div>
             <br>
-            <div class="form-group">  
+            <div class="form-group">
+                <h6><label for="roll">Student Roll</label></h6>
+                <input class="form-control" type="text" name="roll" placeholder="Enter Student Roll" id="roll">
+            </div>
+            <br>
+            <div class="form-group">
                 <input type="submit" class="btn btn-primary" name="submit" value="Add Student">
             </div>
             <br>
-            
+
         </form>
+
+
+
     </div>
 </div>
 
--- lib/Student.php
+++ lib/Student.php
@@ -19,6 +19,19 @@ class Student
         $result = $this->db->select($query);
         return $result; 
     }
+    public function insertStudent($name, $roll){
+        $name = mysqli_real_escape_string($this->db->link, $name);
+        $roll = mysqli_real_escape_string($this->db->link, $roll);
+
+        if(empty($name)|| empty($roll)){
+            $message = "<span class='error'>Field Must not be Empty.</span>";
+            return $message; 
+        }else{
+
+        }
+
+         
+    }
 
 }
 
